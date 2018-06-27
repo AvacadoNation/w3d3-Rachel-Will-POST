5141a0ccc99a924609cfc132d1d121ccd0835ed6
William Holcombe <william.holcombe@uconn.edu>
2018-06-27 12:20:03 -0400

    added POST endpoint for recipes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-
+'use strict';
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {ShoppingList, Recipes} = require('./models');
+
+
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -43,9 +45,28 @@ app.post('/shopping-list', jsonParser, (req, res) => {
     }
   }
 
-  const item = ShoppingList.create(req.body.name, req.body.budget);
+ 
+  const item = ShoppingList.create(req.body.name, req.body.ingredients);
   res.status(201).json(item);
 });
+
+
+
+app.post('/recipes', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
+
 
 
 app.get('/recipes', (req, res) => {
